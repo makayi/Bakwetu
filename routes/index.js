6f8609a5bd83702c072f005f9798a71ff1c0963f
mbuyu <mbmakayi@gmail.com>
2017-01-06 14:56:15 +0200

    Added the ability to login, register and view the basic user information, Also deleted the pages folder. Updated the log file.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 var path = require('path');
 var User_model= require('../models/user.js');
 var formidable = require('formidable');
+var jwt=require('jwt-simple');
 
+var config= require('../config/database.js');
+var passport= require('passport');
+// pass passport for configuration
+require('../config/passport')(passport);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
@@ -11,27 +16,7 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/create', function(req,res,next){
-//   var firstname,lastname,password;
-//   var form = new formidable.IncomingForm();
-//   form.parse(req);
-//   form.on('field',function(name,value){
-//
-//    // get the data from the form sent through a body
-//     if(name=="firstname"){
-//       firstname=value;
-//     } else if (name=="lastname") {
-//       lastname=value;
-//     } else if (name=="password") {
-//       password= value;
-//
-//     }
-//
-// console.log(firstname+ "-" + lastname + password);
-//
-//   }
-//
-// );
+router.post('/register', function(req,res,next){
  console.log(req.body.password);
   console.log(req.body.email);
   var user= new User_model({
@@ -43,16 +28,14 @@ router.post('/create', function(req,res,next){
 
   if(user!=null){
     user.save(function(err,user){
-      var response = {
-        status: 200,
-        message: "Post has been saved",
-        post: user
-      };
 
       if(err){
         return res.json(err.message);
       } else{
-        res.json(response);
+        var token = jwt.encode(user, config.secret);
+        // return the information including token as JSON
+        console.log(token);
+        res.json({success: true, token: 'JWT ' + token});
       }
 
     });
@@ -61,5 +44,61 @@ router.post('/create', function(req,res,next){
   }
 
 });
+
+router.post('/authenticate', function(req, res) {
+  console.log(req.body.email + req.body.password);
+ User_model.findOne({
+    email: req.body.email
+  }, function(err, user) {
+    if (err) throw err;
+     console.log(err);
+    if (!user) {
+      res.send({success: false, msg: 'Authentication failed. User not found.'});
+    } else {
+      // if user is found and password is right create a token
+           var token = jwt.encode(user, config.secret);
+           // return the information including token as JSON
+           console.log(token);
+           res.json({success: true, token: 'JWT ' + token});
+
+    }
+  });
+});
+
+
+
+
+router.get('/userinfo',passport.authenticate('jwt',{session:false}),function(req,res){
+  var token =getToken(req.headers);
+
+  if (token){
+    var decoded= jwt.decode(token,config.secret);
+    User_model.findOne({firstname:decoded.firstname},function(err,user){
+      if(err){
+        throw err;
+      }
+      if(!user){
+        return res.status(403).send({success:false,msg:'Authentication failed'})
+      } else{
+        res.json({sucess:true,msg:'Welcome in the members',user:user});
+      }
+    });
+  } else{
+    return res.status(403).send({sucess:false,msg:'No Token provided.'});
+  }
+});
+
+getToken= function(headers){
+  if(headers&& headers.authorization){
+    var parted= headers.authorization.split(' ');
+    if(parted.length=== 2){
+      return parted[1];
+    } else{
+      return null;
+    }} else{
+      return null;
+    }
+  };
+
 
 module.exports = router;
